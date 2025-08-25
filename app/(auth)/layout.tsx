6e5d39a959461633dab6a671d5478c8d4e588b8d
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-25 20:12:11 +0200

    Add Signin With Github

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,30 @@
+import SocialAuthForm from "@/components/forms/SocialAuthForm";
+import Image from "next/image";
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
-      <section>{children}</section>
+    <main className="flex min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark">
+      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex items-center justify-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="h2-bold text-dark100_light900">Join EasyDevFlow</h1>
+            <p className="paragraph-regular text-dark500_light400">
+              To get Your questions Answered
+            </p>
+          </div>
+          <Image
+            src="/images/site-logo.svg"
+            alt="EasyDevFlow Logo"
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+        </div>
+        {children}
+
+        <SocialAuthForm />
+      </section>
     </main>
   );
 };
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import Image from "next/image";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import React from "react";
 
-export default function Home() {
+const Home = async () => {
+  const session = await auth();
+  console.log("SESSION: ",session);
+
   return (
-    <></>
+    <div>
+    
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out </Button>
+      </form>
+    </div>
   );
-}
+};
+
+export default Home;
