0b0599e8d55c80bfb275d75e19917f5a26fd7e00
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-26 18:20:10 +0200

    Question Card Page Setup

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -13,7 +13,7 @@ const QuestionCard = ({
   question: { _id, title, tags, author, upvotes, answers, views, createdAt },
 }: Props) => {
   return (
-    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11" >
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
