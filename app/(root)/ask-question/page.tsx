e5062aac2174b9f41a9c3265d806f42712160106
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-26 20:30:36 +0200

    Ask Question Page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import QuestionForm from "@/components/forms/QuestionForm";
 import React from "react";
 
 const AskAQuestion = () => {
   return (
     <div>
-      <h1>AskAQuestion</h1>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
     </div>
   );
 };
