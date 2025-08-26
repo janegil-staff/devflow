96fe13edb9a5c54bfe79917c5442cd8c6f42d510
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-26 21:13:23 +0200

    Added models

---
+++ database/interaction.model.ts
@@ -0,0 +1,26 @@
+import { Schema, models, model, Document, Types } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: string;
+}
+
+export interface IInteractionDoc extends IInteraction, Document {}
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: { type: String, required: true }, // 'upvote', 'downvote' 'view' ,'ask_question'
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Interaction =
+  models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
